ab2ac758e4849132fb2e67afa7b83980282004b7
carlitos9889 <carlitos9889gitig@gmail.com>
2023-09-06 23:15:31 -0400

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		fontFamily: {
+			custom: ["DM Sans"],
+		},
+		colors: {
+			bitcoinorange: "#f7931a",
+			softorange: "#ffe9d5",
+			secondaryblue: "#1a9af7",
+			softblue: "#e7f5ff",
+			warmblack: "#282623",
+			black: "#201e1c",
+			grey: "#bababa",
+			offwhite: "#faf8f7",
+			justwhite: "#fff",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
